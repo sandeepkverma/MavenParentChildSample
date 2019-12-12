6ba153ae7445155209be5eb181e260906ffdf9b4
sandeepkverma <sandeepsmec038@gmail.com>
2019-12-13 02:28:44 +0530

    init commit

---
+++ demoParent/demoChild1/src/main/java/demoChild1/Test.java
@@ -0,0 +1,16 @@
+package demoChild1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("hello");
+		System.out.println("hello");
+	}
+
+}
